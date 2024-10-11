e0f7618a346e0247495d6082daaa933fe9baf27e
LONGMANKE <mburunjoroge0@gmail.com>
2024-10-11 11:11:10 +0300

    dsd

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -4,17 +4,21 @@ Method to determine if all boxes can be opened
 Using prototype: def canUnlockAll(boxes)
 """
 
-
 def canUnlockAll(boxes):
     """
-    Check if boxes can be unlocked
+    Check if all boxes can be unlocked
     """
-    for key in range(1, len(boxes) - 1):
-        ctr = False
-        for idx in range(len(boxes)):
-            ctr = (key in boxes[idx] and key != idx)
-            if ctr:
-                break
-        if ctr is False:
-            return ctr
-    return True
+    opened_boxes = set()
+    opened_boxes.add(0)  # Start with the first box
+    stack = [0]  # Initialize the stack with the first box
+    
+    while stack:
+        current_box = stack.pop()  # Get the last box from the stack
+        # Iterate through the keys in the current box
+        for key in boxes[current_box]:
+            if key not in opened_boxes and key < len(boxes):
+                opened_boxes.add(key)  # Mark this box as opened
+                stack.append(key)  # Add this box to the stack to explore later
+                
+    # If the size of opened_boxes is equal to the number of boxes, return True
+    return len(opened_boxes) == len(boxes)
